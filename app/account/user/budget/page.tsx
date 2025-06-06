e52c96e50482b0c450b753a13f1624b7ba6b1577
aonyango <odhiambo.allan@eclectics.io>
2025-06-06 10:03:42 +0300

    add periods for budgets

--- app/account/user/budget/page.tsx
+++ app/account/user/budget/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import AppBar from "@/app/components/AppBar";
 import BudgetComponent from "@/app/components/BudgetComponent";
+
 import React from "react";
 import { GrTrain } from "react-icons/gr";
 
@@ -43,28 +44,37 @@ const BudgetPage = () => {
       budgets: [
         {
           name: "Transport",
-          allocation: 5000,
-          expenditure: 2000,
+          allocation: 6000,
+          expenditure: 4000,
           start: "2025-05-01",
           color: "bg-red-400",
           end: "2025-05-31",
-          category: "Transport",
+          category: "RENT",
         },
         {
           name: "Upkeep Food",
-          allocation: 5000,
+          allocation: 8000,
           color: "bg-blue-400",
-          expenditure: 4500,
-          category: "Food",
+          expenditure: 3500,
+          category: "FOOD",
           start: "2025-04-01",
           end: "2023-04-30",
         },
         {
           name: "Emergency",
-          allocation: 3000,
+          allocation: 5000,
           color: "bg-red-400",
           expenditure: 1500,
-          category: "General",
+          category: "ELECTRICITY",
+          start: "2025-05-01",
+          end: "2025-05-31",
+        },
+        {
+          name: "Emergency",
+          allocation: 1000,
+          color: "bg-red-400",
+          expenditure: 300,
+          category: "EMERGENCY",
           start: "2025-05-01",
           end: "2025-05-31",
         },
@@ -72,19 +82,30 @@ const BudgetPage = () => {
     },
   ];
   const [activeBudgets, setBudgets] = React.useState(budgets[0].budgets);
+  const [activeMonth, setActiveMonth] = React.useState<number | null>(0);
 
   return (
     <>
       <AppBar title="Budgets" icon={<GrTrain />} />
 
-      <div className="flex flex-col md:flex-row gap-4 my-4 md:w-10/12 md:mx-auto w-full justify-center items-center">
+      <div className="flex  md:flex-row gap-4 my-4 md:w-10/12 md:mx-auto w-full justify-start md:flex-center  ml-2 px-5 items-center overflow-x-hidden">
         {budgets.map((budget, index) => (
-          <MonthComponent key={index} month={budget.date}  />
+          <MonthComponent
+            key={index}
+            month={budget.date}
+            index={index}
+            activeMonth={activeMonth}
+            setActiveMonth={setActiveMonth}
+            onClick={() => {
+              setBudgets(budget.budgets);
+              console.log("Budgets:", budget.budgets);
+            }}
+          />
         ))}
       </div>
 
       <div className="p-5 flex flex-col md:w-10/12 md:mx-auto">
-        <div className="flex gap-2">
+        <div className="flex flex-col md:flex-row gap-2">
           <BudgetCard
             color="text-blue-500"
             value={activeBudgets.reduce((acc, budget) => {
@@ -145,10 +166,31 @@ const BudgetCard: React.FC<{ color: string; value: number; total: string }> = ({
   );
 };
 
-const MonthComponent: React.FC<{ month: string }> = ({ month }) => {
+const MonthComponent: React.FC<{
+  month: string;
+  index: number;
+  onClick: () => void;
+  activeMonth: number | null;
+  setActiveMonth: (index: number | null) => void;
+}> = ({ month, index, activeMonth, setActiveMonth, onClick }) => {
+  const handleClick = () => {
+    // Toggle: if current index is active, deactivate it, otherwise activate it
+    setActiveMonth(activeMonth === index ? null : index);
+    console.log("Active Month:", activeMonth === index ? null : index);
+    console.log("Index:", index);
+    onClick();
+  };
+
   return (
-    <div className="bg-white p-1 rounded-full px-5 shadow-sm mb-4">
-      <h2 className="text-md font-semibold text-gray-700 ">{month}</h2>
+    <div
+      onClick={handleClick}
+      className={`p-1 rounded-full px-5 shadow-sm mb-4 hover:bg-gray-100 hover-text-gray-500 transition-all duration-100 cursor-pointer ${
+        activeMonth === index
+          ? "bg-gray-900 hover:text-gray-600 text-white"
+          : "bg-white"
+      }`}
+    >
+      <p>{month}</p>
     </div>
   );
 };
